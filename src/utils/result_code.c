87a4203211a4e7db8ecd08e924f2589822bbde90
dungtran09 <ngaynangnhe@gmail.com>
2025-08-07 22:45:09 +0700

    Update: Setup struct folder and fisrt array datastructures

---
+++ src/utils/result_code.c
@@ -0,0 +1,39 @@
+#include "./result_code.h"
+
+char *Result_ErrorMessage(Result result) {
+  switch (result) {
+  case kSuccess:
+    return "Success";
+  case kNullParameter:
+    return "A required parameter is NULL!";
+  case kFailedMemoryAllocation:
+    return "Failed to allocate memory!";
+  case kArithmeticOverflow:
+    return "Arithmetic Overflow!";
+  case kInvalidIndex:
+    return "Invqlid index!";
+  case kArgumentOutOfRange:
+    return "The specified valiue is outside the range of valid values";
+  case kDependancyError:
+    return "Error code from a dependency!";
+  case kEmpty:
+    return "List is empty!";
+  case kNotFound:
+    return "The requested object was not found!";
+  case KDuplicate:
+    return "The parameter is a duplicate of an exiting item!";
+  case kOutputPointerIsNotNull:
+    return "An output parameter pointer is not null. These are intended to be "
+           "populated by the function";
+  case kSecurityError:
+    return "There was a security related error!";
+  case kSystemError:
+    return "Underlying OS error!";
+  case kOverFlow:
+    return "Overflow - Request exceeds maximun size!";
+  case KUnderFlow:
+    return "Underflow - Request exceeds minimum size!";
+  default:
+    return "Unknow result code!";
+  }
+}
---
+++ src/utils/result_code.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdio.h>
+
+#define PRINT_ERROR(source, result)                                            \
+  {                                                                            \
+    int(fprint_result) = fprintf(stderr, "%s: %s, %s, %s, %d\n", source,       \
+                                 Result_ErrorMessage(result), __FILE__,        \
+                                 __FUNCTION__, __LINE__);                      \
+    if ((fprint_result) < 0)                                                   \
+      perror("fprint failed!");                                                \
+  }
+
+typedef enum Result {
+  kSuccess = 0,
+  kNullParameter = -1,
+  kFailedMemoryAllocation = -2,
+  kArithmeticOverflow = -3,
+  kInvalidIndex = -4,
+  kArgumentOutOfRange = -5,
+  kDependancyError = -6,
+  kEmpty = -7,
+  kNotFound = -8,
+  KDuplicate = -9,
+  kOutputPointerIsNotNull = -10,
+  kSecurityError = -11,
+  kSystemError = -12,
+  kOverFlow = -13,
+  KUnderFlow = -14,
+} Result;
+
+typedef Result ResultCode;
+
+char *Result_ErrorMessage(Result);
